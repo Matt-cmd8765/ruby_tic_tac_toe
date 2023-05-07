ab0bd270f3f265729e021369dfc6c54993d44269
Matt Marini <mattmarini8765@gmail.com>
2023-05-07 21:39:42 +1000

    Made more object oriented, still not completely DRY

--- main.rb
+++ main.rb
@@ -1,19 +1,18 @@
 # hash to store all moves
-board_hash = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9' }
+hash = { 1 => '1', 2 => '2', 3 => '3', 4 => '4', 5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9' }
 
 # player options
 class Player
-  attr_reader :name, :symbol, :player2_symbol
+  attr_reader :name, :symbol
 
   def initialize(name)
     @name = name
   end
 
   # ask player 1 if they want to play x's or Os
-  def symbol
+  def player_symbol
     @symbol = nil
-    until @symbol == 'X' or @symbol == 'O'
-      puts "Pick X's or O's"
+    until @symbol == 'X' || @symbol == 'O'
       sym = gets.chomp
       @symbol = sym.upcase
     end
@@ -21,31 +20,42 @@ class Player
   end
 
   # player 2 takes other symbol
-  def player2_symbol(player1_symbol)
-    player1_symbol == 'X' ? 'O' : 'X'
+  def assign_player2_symbol(player1_symbol)
+    @symbol = nil
+    if player1_symbol == 'X'
+      @symbol = 'O'
+    elsif player1_symbol == 'O'
+      @symbol = 'X'
+    end
   end
 
   # get move
-  def move
-    puts 'Enter your move'
-    gets.chomp
+  def move(hash)
+    puts "#{@name} enter your move"
+    move = gets.chomp
+    until hash[move.to_i] != 'X' && hash[move.to_i] != 'O'
+      puts 'That square is taken, try again'
+      move = gets.chomp
+    end
+    hash[move.to_i] = @symbol
   end
 end
 
 # Play the game
-
-class Game 
+class Game
   # Get info from the players
-  def get_info
+  attr_reader :player1, :player2
+
+  def info
     puts 'Player 1 name:'
     player1_name = gets.chomp
     puts 'Player 2 name:'
     player2_name = gets.chomp
-    player1 = Player.new(player1_name)
-    player2 = Player.new(player2_name)
-    puts "Player 1 choose X's or O's"
-    player1_symbol = player1.symbol
-    player2_symbol = player2.player2_symbol(player1_symbol)
+    @player1 = Player.new(player1_name)
+    @player2 = Player.new(player2_name)
+    puts "#{player1.name} choose X's or O's"
+    player1_symbol = player1.player_symbol
+    player2.assign_player2_symbol(player1_symbol)
   end
 
   # Shows the Tic-Tac-Toe board
@@ -57,84 +67,70 @@ class Game
     puts " #{hash[7]} | #{hash[8]} | #{hash[9]}  "
   end
 
-
-  def play
-
-  end
-def game(hash)
-  puts "Let's play Tic-Tac-Toe!"
-  puts 'First tell me your name!'
-  player_name = gets.chomp
-  board = Board.new
-  matt = Player.new(player_name)
-  comp = Player.new('Computer')
-  symbol = matt.symbol
-  sym = comp.comp_symbol(symbol)
-  board.show_board(hash)
-
-  until winner?(hash)
-    move = matt.move
-    until hash[move.to_i] != 'X' && hash[move.to_i] != 'O'
-      move = matt.move
-    end
-    hash[move.to_i] = symbol
-    if winner?(hash)
-      board.show_board(hash)
-      puts '#########################'
-      puts "#{matt.name} wins!!!!"
-      puts '#########################'
-      break
-    end
-    mov = comp.comp_move
-    until hash[mov.to_i] != 'X' && hash[mov.to_i] != 'O'
-      mov = comp.comp_move
-    end
-    hash[mov.to_i] = sym
-    board.show_board(hash)
-    if winner?(hash)
-      puts ':(:(:(:(:(:(:(:(:(:('
-      puts "#{comp.name} wins... Better luck next time!"
-      puts ':(:(:(:(:(:(:(:(:(:('
-      break
+  # winner logic. Yes it is not DRY but I'm learning dammit
+  def winner?(hash)
+    if hash[1] == 'X' && hash[2] == 'X' && hash[3] == 'X'
+      true
+    elsif hash[4] == 'X' && hash[5] == 'X' && hash[6] == 'X'
+      true
+    elsif hash[7] == 'X' && hash[8] == 'X' && hash[9] == 'X'
+      true
+    elsif hash[1] == 'X' && hash[4] == 'X' && hash[7] == 'X'
+      true
+    elsif hash[2] == 'X' && hash[5] == 'X' && hash[8] == 'X' 
+      true
+    elsif hash[3] == 'X' && hash[6] == 'X' && hash[9] == 'X'
+      true
+    elsif hash[1] == 'X' && hash[5] == 'X' && hash[9] == 'X'
+      true
+    elsif hash[3] == 'X' && hash[5] == 'X' && hash[7] == 'X'
+      true
+    elsif hash[1] == 'O' && hash[2] == 'O' && hash[3] == 'O'
+      true
+    elsif hash[4] == 'O' && hash[5] == 'O' && hash[6] == 'O'
+      true
+    elsif hash[7] == 'O' && hash[8] == 'O' && hash[9] == 'O'
+      true
+    elsif hash[1] == 'O' && hash[4] == 'O' && hash[7] == 'O'
+      true
+    elsif hash[2] == 'O' && hash[5] == 'O' && hash[8] == 'O'
+      true
+    elsif hash[3] == 'O' && hash[6] == 'O' && hash[9] == 'O'
+      true
+    elsif hash[1] == 'O' && hash[5] == 'O' && hash[9] == 'O'
+      true
+    elsif hash[3] == 'O' && hash[5] == 'O' && hash[7] == 'O'
+      true
     end
   end
-end
 
-# Logic to determine winner
-def winner?(hash)
-  if hash[1] == 'X' && hash[2] == 'X' && hash[3] == 'X'
-    true
-  elsif hash[4] == 'X' && hash[5] == 'X' && hash[6] == 'X'
-    true
-  elsif hash[7] == 'X' && hash[8] == 'X' && hash[9] == 'X'
-    true
-  elsif hash[1] == 'X' && hash[4] == 'X' && hash[7] == 'X'
-    true
-  elsif hash[2] == 'X' && hash[5] == 'X' && hash[8] == 'X' 
-    true
-  elsif hash[3] == 'X' && hash[6] == 'X' && hash[9] == 'X'
-    true
-  elsif hash[1] == 'X' && hash[5] == 'X' && hash[9] == 'X'
-    true
-  elsif hash[3] == 'X' && hash[5] == 'X' && hash[7] == 'X'
-    true
-  elsif hash[1] == 'O' && hash[2] == 'O' && hash[3] == 'O'
-    true
-  elsif hash[4] == 'O' && hash[5] == 'O' && hash[6] == 'O'
-    true
-  elsif hash[7] == 'O' && hash[8] == 'O' && hash[9] == 'O'
-    true
-  elsif hash[1] == 'O' && hash[4] == 'O' && hash[7] == 'O'
-    true
-  elsif hash[2] == 'O' && hash[5] == 'O' && hash[8] == 'O'
-    true
-  elsif hash[3] == 'O' && hash[6] == 'O' && hash[9] == 'O'
-    true
-  elsif hash[1] == 'O' && hash[5] == 'O' && hash[9] == 'O'
-    true
-  elsif hash[3] == 'O' && hash[5] == 'O' && hash[7] == 'O'
-    true
+  # play function. Should be shorter but I'm learning dammit.
+  def play(hash)
+    show_board(hash)
+    counter = 0
+    until winner?(hash)
+      player1.move(hash)
+      show_board(hash)
+      counter += 1
+      if winner?(hash)
+        puts "#{player1.name} wins!"
+        break
+      end
+      if counter == 9
+        puts 'Tie!'
+        break
+      end
+      player2.move(hash)
+      show_board(hash)
+      counter += 1
+      if winner?(hash)
+        puts "#{player2.name} wins!"
+        break
+      end
+    end
   end
 end
 
-game(board_hash)
+game = Game.new
+game.info
+game.play(hash)
